f8c2c5ec08f5caf6f6dd498f4abe4606e8d8b418
Alan Bogusiewicz <alabogus@amazon.com>
2020-05-14 14:25:44 -0700

    Adds state machine string extraction

--- src/stepFunctions/utils/extractFromCfTemplate.ts
+++ src/stepFunctions/utils/extractFromCfTemplate.ts
@@ -1,6 +1,4 @@
 import {
-    DiagnosticSeverity,
-    DocumentLanguageSettings,
     ASTNode,
     ObjectASTNode,
     PropertyASTNode,
@@ -17,8 +15,35 @@ interface IStateMachineData {
     range: vscode.Range
 }
 
+const STATE_MACHINE_TYPES = ['AWS::Serverless::StateMachine', 'AWS::StepFunctions::StateMachine']
+
 export function isObjectNode(node?: ASTNode): node is ObjectASTNode {
     return node?.type === 'object'
+}
+
+export function isPropertyNode(node?: ASTNode): node is PropertyASTNode {
+    return node?.type === 'property'
+}
+
+function getPath(node: ObjectASTNode, path: string[]): PropertyASTNode | null {
+    const pathClone = [...path]
+
+    if (isObjectNode(node)) {
+        const firstOfPath = pathClone.shift()
+        const pathProp = node.properties.find(prop => prop.keyNode.value === firstOfPath)
+
+        if (pathProp) {
+            if (pathClone.length === 0) {
+                return pathProp
+            }
+
+            if (isObjectNode(pathProp.valueNode)) {
+                return getPath(pathProp.valueNode, pathClone)
+            }
+        }
+    }
+
+    return null
 }
 
 export default function extractStateMachinesFromCfTemplate(textDocument: vscode.TextDocument): IStateMachineData[] {
@@ -39,7 +64,7 @@ export default function extractStateMachinesFromCfTemplate(textDocument: vscode.
                 if (isObjectNode(resourceValueNode)) {
                     const stateMachine = resourceValueNode.properties.find(item => {
                         const isTypeProp = item.keyNode.value === 'Type'
-                        const isStateMachineValue = item.valueNode?.value === 'AWS::Serverless::StateMachine'
+                        const isStateMachineValue = STATE_MACHINE_TYPES.includes(item.valueNode?.value as any)
 
                         return isTypeProp && isStateMachineValue
                     })
@@ -56,8 +81,17 @@ export default function extractStateMachinesFromCfTemplate(textDocument: vscode.
                 const startPos = textDocument.positionAt(offset)
                 const endPos = textDocument.positionAt(offset + colonOffset!)
 
+                let definition = ''
+
+                if (isObjectNode(stateMachineNode.valueNode)) {
+                    const definitionNode = getPath(stateMachineNode.valueNode, ['Properties', 'Definition'])
+                    const defStart = definitionNode?.valueNode?.offset ?? 0
+                    const defEnd = defStart + (definitionNode?.valueNode?.length ?? 0)
+                    definition = text.slice(defStart, defEnd)
+                }
+
                 return {
-                    definition: '',
+                    definition,
                     name: stateMachineNode.keyNode.value,
                     range: new vscode.Range(startPos, endPos),
                 }
